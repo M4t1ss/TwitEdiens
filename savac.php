ad86ca74a51cf8ab6ada9d04d6ac0a17cab3302c
Matiss Rikters <matiss.tk@gmail.com>
2011-10-14 13:34:49 +0300

    izlasimani un viens sīkums

--- savac.php
+++ savac.php
@@ -11,16 +11,9 @@
 			'content'	=>	'track='.WORDS_TO_TRACK,
 		)
 	);
-	//SQL pieslēgšanās informācija
-	$db_server = "localhost";
-	$db_database = "twitter_alerts";
-	$db_user = "root";
-	$db_password = "root";
 
-	//pieslēdzamies SQL serverim
-	$connection = @mysql_connect($db_server, $db_user, $db_password);
-	mysql_set_charset("utf8", $connection);
-	mysql_select_db($db_database);
+	//Pieslēgums DB
+	include "init_sql.php";
 
 	$context = stream_context_create($opts);
 	while (1){
