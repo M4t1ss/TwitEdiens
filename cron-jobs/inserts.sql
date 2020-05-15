44c6b73ed3c4d3bb3294e13e64f7766e7d223ba7
Matīss Rikters <matiss_rikters@hotmail.com>
2020-05-15 13:13:54 +0900

    Tikai iepriekšējo dienu nevis intervālu kopš izsaukšanas brīža

--- cron-jobs/inserts.sql
+++ cron-jobs/inserts.sql
@@ -1,23 +1,15 @@
 INSERT INTO baumuin_twitediens.tweets (id, text, screen_name, created_at, geo, emo)
 	SELECT id, text, screen_name, created_at, geo, emo FROM baumuin_food.tweets
-	WHERE tweets.created_at
-	BETWEEN now( ) - INTERVAL 24 HOUR
-	AND now( );
+	WHERE DATE(tweets.created_at) = DATE(NOW() - INTERVAL 1 DAY)
 INSERT INTO baumuin_twitediens.words (vards, nominativs, tvits, irediens, grupa, eng, datums)
 	SELECT  vards, nominativs, tvits, irediens, grupa, eng, datums FROM baumuin_food.words
-	WHERE words.datums
-	BETWEEN now( ) - INTERVAL 24 HOUR
-	AND now( );
+	WHERE DATE(words.datums) = DATE(NOW() - INTERVAL 1 DAY)
 INSERT INTO baumuin_twitediens.mentions (tweet_id,screen_name,mention,date)
 	SELECT tweet_id,screen_name,mention,date FROM baumuin_food.mentions
-	WHERE mentions.date
-	BETWEEN now( ) - INTERVAL 24 HOUR
-	AND now( );
+	WHERE DATE(mentions.date) = DATE(NOW() - INTERVAL 1 DAY)
 INSERT INTO baumuin_twitediens.media (tweet_id, media_url, expanded_url, date)
 	SELECT tweet_id, media_url, expanded_url, date FROM baumuin_food.media
-	WHERE media.date
-	BETWEEN now( ) - INTERVAL 24 HOUR
-	AND now( );
+	WHERE DATE(media.date) = DATE(NOW() - INTERVAL 1 DAY)
 DELETE FROM baumuin_twitediens.tweets
 WHERE created_at < CURDATE( ) - INTERVAL 3 MONTH;
 DELETE FROM baumuin_twitediens.words
