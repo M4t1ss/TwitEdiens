4a40e0803b284305bc9bc11609bf9267384277bd
Matīss Rikters <matiss.rikters@tilde.lv>
2020-06-22 17:07:40 +0300

    Print id, author, text for annotated tweets

--- tools/get-annotated.php
+++ tools/get-annotated.php
@@ -7,16 +7,31 @@
 include "../includes/init_sql_latest.php";
 
 if($_GET['type'] == 'nei'){
-	$result= mysqli_query($connection, "SELECT text FROM tweets WHERE emo = 0"); 
+	$result= mysqli_query($connection, "SELECT id, screen_name, text FROM tweets WHERE emo = 0"); 
 }
 if($_GET['type'] == 'poz'){
-	$result= mysqli_query($connection, "SELECT text FROM tweets WHERE emo = 1"); 
+	$result= mysqli_query($connection, "SELECT id, screen_name, text FROM tweets WHERE emo = 1"); 
 }
 if($_GET['type'] == 'neg'){
-	$result= mysqli_query($connection, "SELECT text FROM tweets WHERE emo = -1"); 
+	$result= mysqli_query($connection, "SELECT id, screen_name, text FROM tweets WHERE emo = -1"); 
 }
 
 while($p=mysqli_fetch_array($result)){
-	echo $p["text"]."</br>";
+	echo $p["id"]."\t".$p["screen_name"]."\t".replacements($p["text"])."\n";
 }
 		
+function replacements($text){
+	for ($i=0; $i<10; $i++){
+		$text = str_replace("\r\n", " ", $text);
+		$text = str_replace("  ", " ", $text);
+	}
+	for ($i=0; $i<10; $i++){
+		$text = str_replace("\n", " ", $text);
+		$text = str_replace("  ", " ", $text);
+	}
+	for ($i=0; $i<10; $i++){
+		$text = str_replace("  ", " ", $text);
+	}
+	
+	return $text;
+}
