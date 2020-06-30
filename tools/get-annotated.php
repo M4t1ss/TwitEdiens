8c199a5287ab76df39a77a839a33ab99a41339f6
Matīss Rikters <matiss_rikters@hotmail.com>
2020-06-30 15:37:29 +0900

    Print JSON

--- tools/get-annotated.php
+++ tools/get-annotated.php
@@ -14,12 +14,43 @@ if($_GET['type'] == 'poz'){
 }
 if($_GET['type'] == 'neg'){
 	$result= mysqli_query($connection, "SELECT id, screen_name, text FROM tweets WHERE emo = -1"); 
+}else{
+	$result= mysqli_query($connection, "SELECT emo, id, screen_name, text FROM tweets WHERE emo IS NOT NULL"); 
+	
+	echo "[</br>";
+	while($p=mysqli_fetch_array($result)){
+		switch($p["emo"]){
+			case -1:
+				$sentiment = "neg";
+				break;
+			case 0:
+				$sentiment = "neu";
+				break;
+			case 1:
+				$sentiment = "pos";
+				break;
+		}
+		
+		echo "&nbsp;&nbsp;&nbsp;{</br>";
+		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"sentiment":"'.$sentiment.'",</br>';
+		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"screen_name":"'.$p["screen_name"].'",</br>';
+		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tweet_id":"'.$p["id"].'",</br>';
+		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tweet_text":"'.json_replacements(replacements($p["text"])).'"</br>';
+		echo "&nbsp;&nbsp;&nbsp;},</br>";
+	}
+	echo "]";
+	die;
 }
 
 while($p=mysqli_fetch_array($result)){
 	echo $p["id"]."\t".$p["screen_name"]."\t".replacements($p["text"])."\n";
 }
 		
+function json_replacements($text){
+	$text = str_replace('\\', '\\\\"', $text);
+	$text = str_replace('"', '\"', $text);
+	return $text;
+}
 function replacements($text){
 	for ($i=0; $i<10; $i++){
 		$text = str_replace("\r\n", " ", $text);
