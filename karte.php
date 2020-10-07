6138848e7bcd99c9f0a35661867af732d7285168
Matīss Rikters <matiss_rikters@hotmail.com>
2020-10-07 12:49:08 +0900

    This was long overdue...

--- karte.php
+++ karte.php
@@ -1,79 +1,8 @@
 <?php
+include "includes/laiks.php";
 // Turn off all error reporting
 error_reporting(0);
-
-//pirms cik dienām bija pirmais tvīts?
-$die = mysqli_query($connection, "SELECT min( created_at ) diena FROM tweets");
-$mdie=mysqli_fetch_array($die);
-$laiks=strtotime($mdie['diena']);
-$laiks=date("U", $laiks);
-$seconds = time() - $laiks;
-$days = ceil($seconds / 60 / 60 / 24);
-
-//pirms cik dienām bija jaunākais tvīts?
-$die = mysqli_query($connection, "SELECT max( created_at ) diena FROM tweets");
-$mdie=mysqli_fetch_array($die);
-$laiks=strtotime($mdie['diena']);
-$laiks=date("U", $laiks);
-$seconds = time() - $laiks;
-$maxdays = floor($seconds / 60 / 60 / 24);
-
-if(isset($_POST['submit'])) //ja piespiests parādīt
-{
-	header('Content-type: text/HTML; charset=utf-8');
-   //ievācam visus mainīgos
-   $no = strip_tags($_POST['from']);
-   $lidz = strip_tags($_POST['to']);
-	if($no==""){
-	$no=date("y-m-d");
-	}
-	if($lidz==""){
-	$lidz=date("y-m-d");
-	}
-	if($no==$lidz) {
-		$no--;
-		$no = date("y-m-d",strtotime($no."-24 hours"));
-	}
-	$ns=strtotime($no);
-	$ls=strtotime($lidz);
-	if($ns>$ls){
-	$x=$no;
-	$no=$lidz;
-	$lidz=$x;
-	}
-	$no=strtotime($no);
-	$no=date("Y-m-d", $no);
-	$lidz=strtotime($lidz);
-	$lidz=date("Y-m-d", $lidz);
-
-}else{//ja ne, lai parādās pēdējā mēneša dati...
-	$lidz =date("y-m-d", $laiks);
-	$no = date("y-m-d", strtotime("-1 months", $laiks));
-}
-	$nn=strtotime($no);
-	$nn=date("d-m-Y", $nn);
-	$ll=strtotime($lidz);
-	$ll=date("d-m-Y", $ll);
 ?>
-<script type="text/javascript">
-$(function() {
-	$( "#from, #to" ).datepicker({ minDate: -<?php echo $days; ?>, maxDate: -<?php echo $maxdays; ?> });
-	var dates = $( "#from, #to" ).datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		onSelect: function( selectedDate ) {
-			var option = this.id == "from" ? "minDate" : "maxDate",
-				instance = $( this ).data( "datepicker" ),
-				date = $.datepicker.parseDate(
-					instance.settings.dateFormat ||
-					$.datepicker._defaults.dateFormat,
-					selectedDate, instance.settings );
-			dates.not( this ).datepicker( "option", option, date );
-		}
-	});
-	$( "#from, #to" ).datepicker($.datepicker.regional['fr']);
-});
-</script>
 <h2 style='margin:auto auto; text-align:center;'>Twitter gardēžu karte</h2>
 <h5 style='margin:auto auto; text-align:center;font-size:12px;'>
 <form method="post" action="?id=karte">
