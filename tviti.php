a93aa412f25c85f500bff0cf14fe54695685e621
Matīss Rikters <matiss_rikters@hotmail.com>
2020-12-19 00:06:48 +0900

    Citātu attēlošana dzīvajā joslā

--- tviti.php
+++ tviti.php
@@ -10,6 +10,15 @@ while($p=mysqli_fetch_array($latest)){
 	$username = $p["screen_name"];
 	$text = $p["text"];
 	$ttime = $p["created_at"];
+	$quoted_id = $p["quoted_id"];
+	$quoted_text = NULL;
+	
+	if($quoted_id != NULL){
+		$quoted = mysqli_query($connection, "SELECT text, screen_name FROM tweets WHERE id = $quoted_id");
+		$qq=mysqli_fetch_array($quoted);
+		$quoted_text = $qq["text"];
+		$quoted_screen_name = $qq["screen_name"];
+	}
 		
 	$automatic = classify($text);
 	switch ($automatic){
@@ -45,6 +54,12 @@ while($p=mysqli_fetch_array($latest)){
 <div style="<?php if ((time()-StrToTime($ttime))<5){echo"opacity:".((time()-StrToTime($ttime))/5).";";}?>" class="tweet">
 	<div class="lietotajs" style="border-bottom: 0.18em dashed <?php echo $color; ?>;"><?php echo '<a style="text-decoration:none;color:#658304;" href="/draugs/'.trim($username).'">@'.trim($username).'</a> ';?> ( <?php echo $ttime;?> )</div>
 <?php echo $text."<br/>";
+
+if(isset($quoted_text) && strlen($quoted_text) > 0){
+	echo "<div style='border:1px dotted #000; border-radius:5px; padding:2px;'><small>";
+	echo '<a style="text-decoration:none;color:#658304;" href="/draugs/'.str_replace('@','',trim($quoted_screen_name)).'">@'.trim($quoted_screen_name).'</a>: ';
+	echo $quoted_text."</small></div><br/>";
+}
 ?><br/>
 </div>
 <?php
