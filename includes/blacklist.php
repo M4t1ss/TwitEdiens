734c1ad06369e591b17c414613aca6267d9c7694
Matīss Rikters <matiss_rikters@hotmail.com>
2021-04-07 11:58:58 +0900

    Blacklist update

--- includes/blacklist.php
+++ includes/blacklist.php
@@ -8,7 +8,7 @@ $blacklist = array(
 	'GalasServiss', 'portals_santa','KJ_Sievietem', '1188', 'brevings', 'RIGATV24', 'brooklynpubriga', 
 	'RestoransChat', 'beerhouseNo1', 'EgilsDambis1', 'Skrundas_novads', 'dievietelv', 'flowsnet_com', 
 	'CafeOsiris', 'Kalnciemaiela', 'LV_portals', 'lsmlv', 'SakuraSushiBars', 'visidarbi', 'LifeHackslv', 
-	'irLV', 'LIIA_LV', 'latvijasbizness'
+	'irLV', 'LIIA_LV', 'latvijasbizness', 'ofija_'
 );
 
 //Šie konti ir priduraki - tos noteikti @nepieminēt :)
